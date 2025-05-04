d534ad3c3f20d6c2b0f55f7dae5899549d8a3aa7
amauget <amauget@hotmail.com>
2025-05-04 08:52:55 -0700

    add display toggle to hide delete btn if no files selected

--- public/scripts/deleteHandlers.js
+++ public/scripts/deleteHandlers.js
@@ -14,11 +14,31 @@ function toggleRadioDelete(event){
         radio.checked = true
     }
     toggleLabelColor(radio, event)
+
+    evalSelections(event) //hides "delete btn if no files are selected"
 }
+
+function evalSelections(event){
+    const form = event.target.parentNode.parentNode.parentNode
+    const radios = form.querySelectorAll('.fileRadio')
+    const deleteBtn = (form.parentNode).querySelector('#deleteBtn')
+    console.log(deleteBtn)
+    let checkedRadios = false
+
+    for(let i = 0; i < radios.length; i++){
+        if(radios[i].checked === true){
+            checkedRadios = true
+            break
+        }
+    }
+        
+    return checkedRadios === true ? deleteBtn.style.display = 'block' : deleteBtn.style.display = 'none'
+}
+
+
 
 function toggleLabelColor(radio, event){
     const label = event.target.parentNode.querySelector('label')
-
     if(radio.checked){
         return label.style.color = 'red'
     }
@@ -46,7 +66,6 @@ function renderAreYouSure(event){
 function hideAreYouSure(){
     const deleteList = document.querySelector('.deleteList')
     const blocker = document.querySelector('.blocker')
-
     deleteList.innerHTML = ''
     blocker.style.display = 'none'
 }
